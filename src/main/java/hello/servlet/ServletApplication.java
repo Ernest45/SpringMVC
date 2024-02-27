a818574a8193938c8ade6c4de81fa482006ce5d7
Ernest45 <gkgkswns71@gmail.com>
2024-02-27 23:10:13 +0900

    서블릿

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
 
-@ServletComponentScan // 서블릿 자동등록
+@ServletComponentScan // 서블릿 자동등록 스프링이 자동으로 현재 내 패키지 하위의 서블릿을 자동으로 등록해줌
 @SpringBootApplication
 public class ServletApplication {
 
